e08336ba720f645fbe438f13418961ab5ad38d4d
parsaghdr <166321984+parsaghdr@users.noreply.github.com>
2024-04-07 20:39:44 +0330

    Add files via upload

---
+++ CUBE.py
@@ -0,0 +1,46 @@
+import turtle
+tur = turtle.Turtle()
+
+tur.color('green')
+tur.shape('triangle')
+
+
+
+
+for i in range(1):
+    tur.forward(200)
+    tur.left(90)
+    tur.forward(200)
+    tur.left(90)
+    tur.forward(200)
+    tur.left(90)
+    tur.forward(200)
+    tur.right(135)
+    tur.forward(200)
+    tur.right(45)
+    tur.forward(200)
+    tur.right(135)
+    tur.forward(200)
+    tur.left(45)
+    tur.forward(200)
+    tur.left(135)
+    tur.forward(200)
+    tur.left(45)
+    tur.forward(200)
+    tur.left(90)
+    tur.forward(200)
+    tur.left(90)
+    tur.forward(200)
+    tur.right(45)
+    tur.forward(200)
+    tur.left(135)
+    tur.forward(200)
+    tur.left(45)
+    tur.forward(200)
+    tur.left(135)
+    tur.forward(200)
+    
+
+    
+    
+    tur.speed(1000000)
